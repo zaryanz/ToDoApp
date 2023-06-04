3ccc660831bbf32122e11d40bb3b310e2a146d7c
Aryan Singh <aryanx404@gmail.com>
2023-06-04 18:19:47 +0530

    fix: style improvements

--- ToDoApp/Views/TasksView.swift
+++ ToDoApp/Views/TasksView.swift
@@ -15,24 +15,30 @@ struct TasksView: View {
                 .font(.title3).bold()
                 .padding()
                 .frame(maxWidth: .infinity, alignment: .leading)
-            
-            List {
-                ForEach(realmManager.tasks, id: \.id) {
-                    task in
-                    if !task.isInvalidated {
-                        TaskRow(task: task.title, completed: task.completed).onTapGesture {
-                            realmManager.updateTask(id: task.id, completed: !task.completed)
-                        }
-                        .swipeActions(edge: .trailing) {
-                            Button(role: .destructive) {
-                                realmManager.deleteTask(id: task.id)
-                            } label: {
-                                Label("Delete", systemImage: "trash")
+            if !realmManager.tasks.isEmpty {
+                List {
+                    ForEach(realmManager.tasks, id: \.id) {
+                        task in
+                        if !task.isInvalidated {
+                            TaskRow(task: task.title, completed: task.completed).onTapGesture {
+                                print("tap gesture trigerred")
+                                realmManager.updateTask(id: task.id, completed: !task.completed)
+                            }
+                            .swipeActions(edge: .trailing) {
+                                Button(role: .destructive) {
+                                    realmManager.deleteTask(id: task.id)
+                                } label: {
+                                    Label("Delete", systemImage: "trash")
+                                }
                             }
                         }
-                    } 
-                }
+                    }
+                }            .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
+                    .scrollContentBackground(.hidden)
+            } else {
+                Color.clear
             }
+
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
