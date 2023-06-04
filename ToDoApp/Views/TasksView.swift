2fc6689aa835ca00d3e0ecfc7a0c95a57c01ae10
Aryan Singh <aryanx404@gmail.com>
2023-06-04 11:26:31 +0530

    feat: basic project setup

---
+++ ToDoApp/Views/TasksView.swift
@@ -0,0 +1,27 @@
+//
+//  TasksView.swift
+//  ToDoApp
+//
+//  Created by Aryan Singh on 04/06/23.
+//
+
+import SwiftUI
+
+struct TasksView: View {
+    var body: some View {
+        VStack {
+            Text("My Tasks")
+                .font(.title3).bold()
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
+    }
+}
+
+struct TasksView_Previews: PreviewProvider {
+    static var previews: some View {
+        TasksView()
+    }
+}
